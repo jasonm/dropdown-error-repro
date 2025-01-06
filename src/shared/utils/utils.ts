35550a52cd6b9bc5fff673b10ee56ee43f8808b2
Jason Morrison <jason.p.morrison@gmail.com>
2025-01-06 14:06:19 -0800

    SelectBox component and SelectBoxUsage attempt to repro

---
+++ src/shared/utils/utils.ts
@@ -0,0 +1,13 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function arrayEquals(a?: string[], b?: string[]): boolean {
+  if (a === b) return true;
+  if (!a || !b) return false;
+  if (a.length !== b.length) return false;
+  return a.every((val, index) => val === b[index]);
+}
